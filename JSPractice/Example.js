c5a4fc81481ff97d5c6f97d555211f943b209bc9
Andy Humble <humbleandrew13@gmail.com>
2017-03-21 05:35:47 -0400

    initial

---
+++ JSPractice/Example.js
@@ -0,0 +1,7 @@
+﻿//function updateDiv() {
+//    document.getElementById("result").innerHTML = "I cannot believe you just did that... *sigh*";
+//}
+
+function changeImage(ImageID, ImageFileName) {
+    document.getElementById(ImageID).src = ImageFileName;
+}
